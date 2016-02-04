0a63e9c4bbbce9f02f9d38b4c45b8d9fe4ba5188
Chris Brahms <cbrahms@palantir.com>
2016-02-04 16:54:34 -0500

    commit

--- src/test/java/com/palantir/RequestAnswerShould.java
+++ src/test/java/com/palantir/RequestAnswerShould.java
@@ -6,9 +6,9 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class RequestAnswerShould {
+    RequestAnswer requestAnswer = new RequestAnswer();
     @Test public void
     when_request_contains_what_is_bigger_should_return_max_value() {
-        RequestAnswer requestAnswer = new RequestAnswer();
         assertThat(requestAnswer.answer("Which of the following numbers is the largest: 419, 16"),
                    is("419"));
 
@@ -16,7 +16,7 @@ public class RequestAnswerShould {
 
     @Test public void
     when_request_is_two_sum() {
-
+        assertThat(requestAnswer.answer("what is the sum of 3 and 5"), is("8"));
     }
 
 }
